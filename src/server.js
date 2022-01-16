fcdd79bb8a82b8ffeca50a79cf1554e41f2e13ed
assafvan <vanunuassaf@gmail.com>
2022-01-16 19:29:18 +0200

    port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
