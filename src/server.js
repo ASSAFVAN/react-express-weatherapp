9a62504eea859d5cb98934e0cdcf092d3f72ba44
assafvan <vanunuassaf@gmail.com>
2022-01-18 23:13:31 +0200

    commit

--- src/server.js
+++ src/server.js
@@ -50,10 +50,9 @@ app.get("/weather", (req, res) => {
     }
   );
 });
-app.use(express.static(path.join(__dirname, "build")));
 
 app.get(
-  "/*",
+  "/",
   (req,
   (res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
